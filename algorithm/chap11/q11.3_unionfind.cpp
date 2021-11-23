9769233ef3d8573cb985a941631a3a151e0f7c02
TMdiesel <cerezzodora2262@icloud.com>
2021-11-23 05:54:59 +0000

    feat : add chap11

---
+++ algorithm/chap11/q11.3_unionfind.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct UnionFind {
+    vector<int> par; // 各頂点の親頂点の番号
+    vector<int> siz; // 各頂点に属する根付き木の頂点数
+    UnionFind(int n) : par(n, -1), siz(n, 1) {}
+
+    int root(int x) {
+        if(par[x] == -1) {
+            return x;
+        } else {
+            return par[x] = root(par[x]);
+        }
+    }
+
+    bool issame(int x, int y) { return root(x) == root(y); }
+
+    bool unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if(x == y)
+            return false;
+        if(siz[x] < siz[y]) {
+            swap(x, y);
+        }
+        par[y] = x;
+        siz[x] += siz[y];
+        return true;
+    }
+
+    int size(int x) { return siz[root(x)]; }
+};
+
+int main() {
+    UnionFind uf(7);
+    uf.unite(1, 2);
+    uf.unite(2, 3);
+    uf.unite(5, 6);
+    cout << uf.issame(1, 3) << endl;
+    cout << uf.issame(2, 5) << endl;
+}
